268c4c99a2582c4e5eb1cd4408e8288063a050b2
ewaeva <104350563+ewaeva@users.noreply.github.com>
2022-06-21 22:44:57 +0100

    creating weather forecast structure

--- src/app.js
+++ src/app.js
@@ -23,6 +23,36 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thursday", "Friday", "Saturday", "Sunday", "Monday"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+        <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+        <i class="fa-solid fa-cloud-sun"></i>
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function displayWeatherDetails(response) {
   document.querySelector("#main-city").innerHTML = `📍 ${response.data.name}`;
   document.querySelector("#temperature").innerHTML = `${Math.round(
@@ -78,3 +108,4 @@ let yourLocationButton = document.querySelector("#your-location-button");
 yourLocationButton.addEventListener("click", getCurrentLocation);
 
 searchCity("Lisbon");
+displayForecast();
